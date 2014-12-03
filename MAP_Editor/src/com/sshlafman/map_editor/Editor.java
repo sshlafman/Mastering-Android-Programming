62a7325c29423f9ddf3a2c5c31240940a6924597
Shymon Shlafman <sshlasfman@ptc.com>
2014-12-03 18:27:00 +0200

    Started filling main resource in Editor activity.

---
+++ MAP_Editor/src/com/sshlafman/map_editor/Editor.java
@@ -0,0 +1,15 @@
+package com.sshlafman.map_editor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Editor extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
